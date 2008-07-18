07c9024f5ea4ce60f481b8089b61d4988e7588fa
Sjors Provoost <sjors@Sjors.(none)>
2008-07-18 12:33:26 +0200

    Added logs directory and pointed demo to altitude-pg.sprovoost.nl

--- example_route_server/server.py
+++ example_route_server/server.py
@@ -33,8 +33,8 @@ routes = [route1]
 def demo(req, route, input, output, server):
   # ULR of altitude server:
   if server == "pg":
-    url_server_root = 'http://altitude-pg/';
-    #url_server_root = 'http://altitude-pg.sprovoost.nl/';
+    #url_server_root = 'http://altitude-pg/';
+    url_server_root = 'http://altitude-pg.sprovoost.nl/';
   elif server == "app":
     #url_server_root = 'http://localhost:8080/';
     url_server_root = 'http://altitude.sprovoost.nl/';
